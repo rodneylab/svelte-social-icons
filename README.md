0ff8f1270ff92735a5a01708b37e2205b77381a6
Rodney Lab <codesign@rodneylab.com>
2021-08-23 19:08:24 +0100

    style: 🛁 format files

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-Social icons for Svelte.  Based on <a aria-label="Open react-social-icons repo on Git Hub" href="https://github.com/jaketrent/react-social-icons">react-social-icons</a> by Jake Trent.
+Social icons for Svelte. Based on <a aria-label="Open react-social-icons repo on Git Hub" href="https://github.com/jaketrent/react-social-icons">react-social-icons</a> by Jake Trent.
 
 To install run
 
 ```
 pnpm install @rodneylab/svelte-social-icons
 ```
-
 
 To use:
 
--- src/SocialIcons.svelte
+++ src/SocialIcons.svelte
@@ -15,7 +15,6 @@
   const socialContainerStyle = 'position: absolute; inset: 0; width: 100%; height: 100%;';
   const socialSvgStyle =
     'border-radius: 50%; position: absolute; inset: 0; width: 100%; height: 100%; fill-rule: evenodd;';
-
   const socialSvgMaskStyle =
     "@media (prefers-reduced-motion: no-preference) { --ms-transition: 'fill 170ms ease-in-out'; -o-transition: 'fill 170ms ease-in-out'; --moz-transition: 'fill 170ms ease-in-out'; --webkit-transition: 'fill 170ms ease-in-out'; transition: 'fill 170ms ease-in-out';}";
   const socialSvgBackgroundStyle = `${socialSvgMaskStyle} fill: transparent;`;
@@ -24,7 +23,9 @@
 <span class="social-icon" style={socialIconStyle}>
   <div class="social-container" style={socialContainerStyle}>
     <svg class="social-svg" viewBox="0 0 64 64" style={socialSvgStyle}>
-      <g class="social-svg-background" style={socialSvgBackgroundStyle}><circle cx="32" cy="32" r="31" /></g>
+      <g class="social-svg-background" style={socialSvgBackgroundStyle}
+        ><circle cx="32" cy="32" r="31" /></g
+      >
       <g class="social-svg-icon" fill={fgColor}>
         <path d={icon} />
       </g>
@@ -32,4 +33,3 @@
     </svg>
   </div>
 </span>
-
