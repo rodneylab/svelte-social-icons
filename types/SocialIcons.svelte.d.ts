160ad88b84606cb79e944de188276e9a2e711517
Rodney Lab <codesign@rodneylab.com>
2022-07-02 06:57:33 +0100

    bump: version 0.0.21 → 0.0.22

--- types/SocialIcons.svelte.d.ts
+++ types/SocialIcons.svelte.d.ts
@@ -1,56 +1,56 @@
 /// <reference types="svelte" />
-import type { SvelteComponentTyped } from 'svelte';
+import type { SvelteComponentTyped } from "svelte";
 
 export interface SocialIconsProps {
   /**
    * @default undefined
    */
-  network?:
-    | 'bandsintown'
-    | 'behance'
-    | 'codepen'
-    | 'discord'
-    | 'dribbble'
-    | 'dropbox'
-    | 'email'
-    | 'facebook'
-    | 'foursquare'
-    | 'github'
-    | 'google'
-    | 'google_play'
-    | 'instagram'
-    | 'itch.io'
-    | 'itunes'
-    | 'linkedin'
-    | 'mailto'
-    | 'medium'
-    | 'meetup'
-    | 'pinterest'
-    | 'rdio'
-    | 'reddit'
-    | 'rss'
-    | 'sharethis'
-    | 'smugmug'
-    | 'snapchat'
-    | 'soundcloud'
-    | 'spotify'
-    | 'squarespace'
-    | 'stackoverflow'
-    | 'telegram'
-    | 'tiktok'
-    | 'tumblr'
-    | 'twitch'
-    | 'twitter'
-    | 'upwork'
-    | 'vevo'
-    | 'vimeo'
-    | 'vine'
-    | 'vk'
-    | 'vsco'
-    | 'wechat'
-    | 'whatsapp'
-    | 'yelp'
-    | 'youtube';
+  network:
+    | "bandsintown"
+    | "behance"
+    | "codepen"
+    | "discord"
+    | "dribbble"
+    | "dropbox"
+    | "email"
+    | "facebook"
+    | "foursquare"
+    | "github"
+    | "google"
+    | "google_play"
+    | "instagram"
+    | "itch.io"
+    | "itunes"
+    | "linkedin"
+    | "mailto"
+    | "medium"
+    | "meetup"
+    | "pinterest"
+    | "rdio"
+    | "reddit"
+    | "rss"
+    | "sharethis"
+    | "smugmug"
+    | "snapchat"
+    | "soundcloud"
+    | "spotify"
+    | "squarespace"
+    | "stackoverflow"
+    | "telegram"
+    | "tiktok"
+    | "tumblr"
+    | "twitch"
+    | "twitter"
+    | "upwork"
+    | "vevo"
+    | "vimeo"
+    | "vine"
+    | "vk"
+    | "vsco"
+    | "wechat"
+    | "whatsapp"
+    | "yelp"
+    | "youtube";
 
   /**
    * @default undefined
@@ -78,4 +78,8 @@ export interface SocialIconsProps {
   style?: string;
 }
 
-export default class SocialIcons extends SvelteComponentTyped<SocialIconsProps, {}, {}> {}
+export default class SocialIcons extends SvelteComponentTyped<
+  SocialIconsProps,
+  {},
+  {}
+> {}
