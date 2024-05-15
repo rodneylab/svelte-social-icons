ce681b984b17840a643174c8667d089fc8487f27
Rodney Lab <codesign@rodneylab.com>
2024-05-15 09:43:43 +0100

    fix: update svelte config

--- types/SocialIcons.svelte.d.ts
+++ types/SocialIcons.svelte.d.ts
@@ -1,55 +1,55 @@
-import type { SvelteComponentTyped } from 'svelte';
+import type { SvelteComponentTyped } from "svelte";
 
 export interface SocialIconsProps {
   /**
    * @default undefined
    */
   network:
-    | 'bandsintown'
-    | 'behance'
-    | 'codepen'
-    | 'discord'
-    | 'dribbble'
-    | 'dropbox'
-    | 'email'
-    | 'facebook'
-    | 'foursquare'
-    | 'github'
-    | 'google'
-    | 'google_play'
-    | 'instagram'
-    | 'itch.io'
-    | 'itunes'
-    | 'linkedin'
-    | 'mailto'
-    | 'medium'
-    | 'meetup'
-    | 'pinterest'
-    | 'rdio'
-    | 'reddit'
-    | 'rss'
-    | 'sharethis'
-    | 'smugmug'
-    | 'snapchat'
-    | 'soundcloud'
-    | 'spotify'
-    | 'squarespace'
-    | 'stackoverflow'
-    | 'telegram'
-    | 'tiktok'
-    | 'tumblr'
-    | 'twitch'
-    | 'twitter'
-    | 'upwork'
-    | 'vevo'
-    | 'vimeo'
-    | 'vine'
-    | 'vk'
-    | 'vsco'
-    | 'wechat'
-    | 'whatsapp'
-    | 'yelp'
-    | 'youtube';
+    | "bandsintown"
+    | "behance"
+    | "codepen"
+    | "discord"
+    | "dribbble"
+    | "dropbox"
+    | "email"
+    | "facebook"
+    | "foursquare"
+    | "github"
+    | "google"
+    | "google_play"
+    | "instagram"
+    | "itch.io"
+    | "itunes"
+    | "linkedin"
+    | "mailto"
+    | "medium"
+    | "meetup"
+    | "pinterest"
+    | "rdio"
+    | "reddit"
+    | "rss"
+    | "sharethis"
+    | "smugmug"
+    | "snapchat"
+    | "soundcloud"
+    | "spotify"
+    | "squarespace"
+    | "stackoverflow"
+    | "telegram"
+    | "tiktok"
+    | "tumblr"
+    | "twitch"
+    | "twitter"
+    | "upwork"
+    | "vevo"
+    | "vimeo"
+    | "vine"
+    | "vk"
+    | "vsco"
+    | "wechat"
+    | "whatsapp"
+    | "yelp"
+    | "youtube";
 
   /**
    * @default undefined
